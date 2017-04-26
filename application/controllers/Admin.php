8ba7205680af69eeef3554bade948736d8b07988
chenjianwei <chenjianwei@chenjianweideMacBook-Pro.local>
2017-04-26 15:13:55 +0800

    no message

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -166,11 +166,13 @@ class Admin extends CI_Controller{
     private function _addFileToZip($path,$zip){
         $handler=opendir($path); //打开当前文件夹由$path指定。
         while(($filename=readdir($handler))!==false){
-            if($filename != "." && $filename != ".."){//文件夹文件名字为'.'和‘..’，不要对他们进行操作
+            if($filename != "." && $filename != ".." &&
+             substr(strrchr($filename, '.'), 1)=="png" 
+            ){//文件夹文件名字为'.'和‘..’，不要对他们进行操作
                 if(is_dir($path."/".$filename)){// 如果读取的某个对象是文件夹，则递归
-                    // _addFileToZip($path."/".$filename, $zip);
+                    _addFileToZip($path."/".$filename, $zip);
                 }else{ //将文件加入zip对象
-                    $zip->addFile($path."/".$filename);
+                    $zip->addFile($filename);
                 }
             }
         }
