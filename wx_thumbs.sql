7a7d4645916dbb564c33a226aa5ed199f392cf58
chenjianwei <chenjianwei@chenjianweideMacBook-Pro.local>
2017-05-04 22:26:50 +0800

    Sql

--- wx_thumbs.sql
+++ wx_thumbs.sql
@@ -1,18 +1,31 @@
--- --------------------------------------------------------
--- 主机:                           127.0.0.1
--- 服务器版本:                        5.6.17 - MySQL Community Server (GPL)
--- 服务器操作系统:                      Win64
--- HeidiSQL 版本:                  9.4.0.5125
--- --------------------------------------------------------
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.6.35)
+# Database: wx_thumbs
+# Generation Time: 2017-05-04 14:25:01 +0000
+# ************************************************************
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- 导出  表 wx_thumbs.admin_users 结构
-CREATE TABLE IF NOT EXISTS `admin_users` (
+
+# Dump of table admin_users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `admin_users`;
+
+CREATE TABLE `admin_users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `login_name` varchar(30) DEFAULT NULL,
   `passwd` varchar(40) DEFAULT NULL,
@@ -21,15 +34,23 @@ CREATE TABLE IF NOT EXISTS `admin_users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wx_thumbs.admin_users 的数据：~1 rows (大约)
-DELETE FROM `admin_users`;
+LOCK TABLES `admin_users` WRITE;
 /*!40000 ALTER TABLE `admin_users` DISABLE KEYS */;
-INSERT INTO `admin_users` (`id`, `login_name`, `passwd`, `permission`, `name`) VALUES
-	(0, 'admin', '4297f44b13955235245b2497399d7a93', NULL, 'admin');
-/*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
 
--- 导出  表 wx_thumbs.comment 结构
-CREATE TABLE IF NOT EXISTS `comment` (
+INSERT INTO `admin_users` (`id`, `login_name`, `passwd`, `permission`, `name`)
+VALUES
+	(1,'admin','21232f297a57a5a743894a0e4a801fc3',NULL,'admin');
+
+/*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table comment
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comment`;
+
+CREATE TABLE `comment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `star` int(11) NOT NULL DEFAULT '0',
   `text` varchar(500) NOT NULL DEFAULT '0',
@@ -39,41 +60,67 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `wx_name` varchar(50) DEFAULT NULL,
   `time` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wx_thumbs.comment 的数据：~0 rows (大约)
-DELETE FROM `comment`;
-/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 
--- 导出  表 wx_thumbs.shop 结构
-CREATE TABLE IF NOT EXISTS `shop` (
+
+# Dump of table shop
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `shop`;
+
+CREATE TABLE `shop` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `des` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wx_thumbs.shop 的数据：~0 rows (大约)
-DELETE FROM `shop`;
+LOCK TABLES `shop` WRITE;
 /*!40000 ALTER TABLE `shop` DISABLE KEYS */;
-/*!40000 ALTER TABLE `shop` ENABLE KEYS */;
 
--- 导出  表 wx_thumbs.staffs 结构
-CREATE TABLE IF NOT EXISTS `staffs` (
+INSERT INTO `shop` (`id`, `name`, `des`)
+VALUES
+	(40,'df',''),
+	(41,'er','');
+
+/*!40000 ALTER TABLE `shop` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table staffs
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `staffs`;
+
+CREATE TABLE `staffs` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `des` text,
   `job` varchar(100) DEFAULT NULL,
   `shop_id` int(11) DEFAULT NULL,
+  `header` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wx_thumbs.staffs 的数据：~0 rows (大约)
-DELETE FROM `staffs`;
+LOCK TABLES `staffs` WRITE;
 /*!40000 ALTER TABLE `staffs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `staffs` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+INSERT INTO `staffs` (`id`, `name`, `des`, `job`, `shop_id`, `header`)
+VALUES
+	(1,'敌方','','',41,NULL),
+	(2,'d','','',41,NULL),
+	(3,'4','','',41,''),
+	(4,'fg','','22',41,'ce579c9e1d37bbf79aee811e96a4db2a.png');
+
+/*!40000 ALTER TABLE `staffs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
